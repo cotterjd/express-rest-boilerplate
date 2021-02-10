0ae88c207fab490a05df13cab872d23ac84e0a11
Jordan Cotter <jrdn.cttr@gmail.com>
2021-02-10 10:13:30 -0600

    feat: add env support for PORT

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json()) // to be able to receive json in requests
 app.get('/:name', controller.getExample)
 app.post('/', controller.postExample)
 
-app.set('port', 3040)
+app.set('port', process.env.PORT || 3040)
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'))
 })
